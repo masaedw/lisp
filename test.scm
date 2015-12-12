b60c534dee92253e47726e2d7b3ff7c8d1f33faa
Masayuki Muto <masa.edw@gmail.com>
2015-12-12 23:04:16 +0900

    Use eqv? to numerical compare

--- test.scm
+++ test.scm
@@ -2,9 +2,9 @@
             (print x)
             (newline)))
 
-(p (if (eq? 1 1)
-     'success_eq_1_1
-     'failed_eq_1_1))
+(p (if (eqv? 1 1)
+     'success_eqv_1_1
+     'failed_eqv_1_1))
 
 (define-macro m
   (lambda (f)
