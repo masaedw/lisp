38d78fb4f7adb049d2824bf48973750a50cae61e
Masayuki Muto <masa.edw@gmail.com>
2015-12-26 22:33:26 +0900

    Comment result of fib benchmark

--- samples/fib-vm.scm
+++ samples/fib-vm.scm
@@ -9,3 +9,15 @@
 
 (print (eval-vm '(fib 30)))
 (newline)
+
+;; first heap based vm version
+;;
+;; $ time ./lisp < samples/fib-vm.scm
+;; 832040
+;; ./lisp < samples/fib-vm.scm  6.37s user 1.59s system 111% cpu 7.102 total
+;;
+;; naive interpreter version
+;;
+;; $ time ./lisp < samples/fib.scm
+;; 832040
+;; ./lisp < samples/fib.scm  5.03s user 1.22s system 115% cpu 5.439 total
