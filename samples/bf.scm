4c81a0ff7f54d3f98225ca927128c14eedec844a
Masayuki Muto <masa.edw@gmail.com>
2016-03-17 00:34:52 +0900

    RLE

--- samples/bf.scm
+++ samples/bf.scm
@@ -43,8 +43,31 @@
 (define i-jump 10) ; jump
 (define i-zero 11) ; set pointer to zero [-]
 
-(define (compile xs)
-  (apply bytevector xs))
+(define (reverse xs)
+  (define (iter x xs)
+    (if (null? xs)
+      x
+      (iter (cons (car xs) x) (cdr xs))))
+  (iter () xs))
+
+(define (bf-rle xs)
+  (define (iter a c x xs)
+    (cond
+     ((null? xs)
+      (reverse (acons a c x)))
+     ((or (= (car xs) bfc-beg) (= (car xs) bfc-end))
+      (iter (car xs) 1 (acons a c x) (cdr xs)))
+     ((= (car xs) a)
+      (iter a (+ c 1) x (cdr xs)))
+     (else
+      (iter (car xs) 1 (acons a c x) (cdr xs)))))
+
+  (if (null? xs)
+    ()
+    (iter (car xs) 1 () (cdr xs))))
+
+(define (bf-compile xs)
+  (vector (bf-rle (bf-parse xs))))
 
 (define pc 0)
 (define p 0)
@@ -52,7 +75,7 @@
 (define mem (make-vector 30000 0))
 
 (define (current-insn prog)
-  (bytevector-u8-ref prog pc))
+  (vector-ref prog pc))
 (define (current-memory)
   (vector-ref mem p))
 (define (set-current-memory! v)
@@ -61,7 +84,7 @@
 (define (jump-forward prog tpc n)
   (if (= n 0)
     (set! pc tpc)
-    (let1 i (bytevector-u8-ref prog (+ tpc 1))
+    (let1 i (car (vector-ref prog (+ tpc 1)))
       (cond
        ((= i bfc-beg)
         (jump-forward prog (+ tpc 1) (+ n 1)))
@@ -73,7 +96,7 @@
 (define (jump-backward prog tpc n)
   (if (= n 0)
     (set! pc tpc)
-    (let1 i (bytevector-u8-ref prog (- tpc 1))
+    (let1 i (car (vector-ref prog (- tpc 1)))
       (cond
        ((= i bfc-end)
         (jump-backward prog (- tpc 1) (+ n 1)))
@@ -82,24 +105,38 @@
        (else
         (jump-backward prog (- tpc 1) n))))))
 
+
+(define (iterate n x)
+  (if (<= n 0)
+    ()
+    (begin
+      (x)
+      (iterate (- n 1) x))))
+
+(define-macro repeat
+  (lambda (n . body)
+    (list 'iterate n (cons 'lambda (cons () body)))))
+
 (define (bf-vm in out prog)
   (define (iter)
-    (if (>= (bytevector-length prog) pc)
-      (let1 i (current-insn prog)
+    (if (>= (vector-length prog) pc)
+      (let ((i (car (current-insn prog)))
+            (c (cdr (current-insn prog))))
+        (repeat c (current-memory))
         (cond
          ((= i bfc-inc)
-          (set! p (+ p 1)))
+          (set! p (+ p c)))
          ((= i bfc-dec)
-          (set! p (- p 1)))
+          (set! p (- p c)))
          ((= i bfc-add)
-          (set-current-memory! (+ (current-memory) 1)))
+          (set-current-memory! (+ (current-memory) c)))
          ((= i bfc-sub)
-          (set-current-memory! (- (current-memory) 1)))
+          (set-current-memory! (- (current-memory) c)))
          ((= i bfc-puc)
-          (write-u8 (current-memory) out))
+          (repeat c (write-u8 (current-memory) out)))
          ((= i bfc-gec)
-          (set-current-memory! (let1 c (read-u8 in)
-                                 (if (eof-object? c) 0 c))))
+          (repeat c (set-current-memory! (let1 c (read-u8 in)
+                                           (if (eof-object? c) 0 c)))))
          ((= i bfc-beg)
           (if (= (current-memory) 0)
             (jump-forward prog pc 1)))
@@ -111,6 +148,6 @@
 
 (define (main args)
   (let1 file (open-input-file (car (cdr args)))
-    (bf-vm (current-input-port) (current-output-port) (apply bytevector (bf-parse file)))))
+    (bf-vm (current-input-port) (current-output-port) (bf-compile file))))
 
 (main (command-line))
